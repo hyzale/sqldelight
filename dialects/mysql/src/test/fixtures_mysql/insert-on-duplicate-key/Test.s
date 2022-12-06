86d83898c412ea815b9b9a2a9e668376b7ec0a24
Alec Strong <AlecStrong@users.noreply.github.com>
2022-12-06 17:55:33 -0500

    Update Test.s

--- dialects/mysql/src/test/fixtures_mysql/insert-on-duplicate-key/Test.s
+++ dialects/mysql/src/test/fixtures_mysql/insert-on-duplicate-key/Test.s
@@ -6,3 +6,8 @@ CREATE TABLE test (
 INSERT INTO test
 VALUES (1,2)
 ON DUPLICATE KEY UPDATE value = VALUES(value);
+
+INSERT INTO test
+  (id, value)
+VALUES (:id, :value)
+ON DUPLICATE KEY UPDATE value = value + VALUES(value);
